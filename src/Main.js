c53d5e7968c165211ea81dfa067824974093f668
Alex Rodriguez <alexrod102797@gmail.com>
2021-07-03 14:27:19 -0400

    First commit, positioning

---
+++ src/Main.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './Styling/Main.css'
+
+class Main extends React.Component{
+   render(){
+    return (
+        <div className='Main'>
+            <div id='about-section'>
+                <div>
+                    <h1>ksjdngfkj;sbgnDDngfNglngmNG;MDNg:KMDNgd;gN;KDMgnsD:KGsdmk</h1>
+                </div>
+                
+                <div>
+                    <h1>nav</h1>
+                </div>
+                    
+                <div id='external-links'>
+                    <h1>social media</h1>
+                </div>
+             </div>
+    
+            <div id='emptyspace'>
+            </div>
+          
+            <div id='avatar'>
+                    <h1>avatar</h1>
+            </div>
+        </div>
+
+      );
+  }  
+}
+
+export default Main;
